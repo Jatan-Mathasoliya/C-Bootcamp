37dcd51b3c3e5f815ab94a0704f771a2b3950a41
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2025-01-02 23:18:48 +0530

    C++

--- 32.cpp
+++ 32.cpp
@@ -1,21 +1,64 @@
 #include <iostream>
 using namespace std;
 #include <string>
-string categories(int age){
-    if((age < 0) || (age > 100)){
-        return "Invalid age";
-    }
-    else if(age < 18){
-        return "Teenager";
-    }
-    else if((age > 18) && (age < 50)){
-        return "Adult";
-    }
-    else{
-        return "Senior";
-    }
-}
-int main(){
-    int age = 42;
-    cout << categories(age);
-}
+
+// 1st Approach -------------------->
+
+// string categories(int age){
+//     if((age < 0) || (age > 100)){
+//         return "Invalid age";
+//     }
+//     else if(age < 18){
+//         return "Teenager";
+//     }
+//     else if((age > 18) || (age < 50)){
+//         return "Adult";
+//     }
+//     else{
+//         return "Senior";
+//     }
+// }
+// int main(){
+//     int age = 20;
+//     cout << categories(age);
+// }
+
+// 2nd Approach ---------------->
+
+// int main(){
+//     int age = -55;
+//     int category = 0;
+//     if((age > 0) && (age < 18)){
+//         category = 1;
+//     }
+//     else if((age >= 18) && (age <= 50)){
+//         category = 2;
+//     }
+//     else if((age > 50) && (age <= 100)){
+//         category = 3;
+//     }
+//     else{
+//         category = 4;
+//     }
+
+//     switch(category){
+//         case 1:
+//         cout << "Teenager";
+//         break;
+//         case 2:
+//         cout << "Adult";
+//         break;
+//         case 3:
+//         cout << "Senior";
+//         break;
+//         default:
+//         cout << "Invalid age";
+//     }
+// }
+
+// 3rd Approach ------------------>
+
+// int main(){
+//     int age = 18;
+//     cout << (age < 18) ? "Teenager" : (age >= 18 && age <=50) ? "Adult" : (age > 50 && age <= 100) ? "Senior" : "Invalid age";
+// }
